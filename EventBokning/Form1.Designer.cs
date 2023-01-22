9ac96ddd3face77901fe32b098819edb034ef6fd
Hbitz <axel_lindstrom_98@hotmail.com>
2023-01-22 19:54:13 +0100

    good commit beep boop

--- EventBokning/Form1.Designer.cs
+++ EventBokning/Form1.Designer.cs
@@ -28,13 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.tbxAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsertNewUser = new System.Windows.Forms.Button();
+            this.lblOutputTest = new System.Windows.Forms.Label();
+            this.btnSelectCustomers = new System.Windows.Forms.Button();
+            this.gridOutput = new System.Windows.Forms.DataGridView();
+            this.gridEventOutput = new System.Windows.Forms.DataGridView();
+            this.btnPerformerWindow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridOutput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEventOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(84, 31);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(100, 20);
+            this.tbxName.TabIndex = 0;
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.Location = new System.Drawing.Point(84, 66);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(100, 20);
+            this.tbxEmail.TabIndex = 1;
+            // 
+            // tbxAge
+            // 
+            this.tbxAge.Location = new System.Drawing.Point(84, 106);
+            this.tbxAge.Name = "tbxAge";
+            this.tbxAge.Size = new System.Drawing.Size(100, 20);
+            this.tbxAge.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Age";
+            // 
+            // btnInsertNewUser
+            // 
+            this.btnInsertNewUser.Location = new System.Drawing.Point(84, 157);
+            this.btnInsertNewUser.Name = "btnInsertNewUser";
+            this.btnInsertNewUser.Size = new System.Drawing.Size(112, 37);
+            this.btnInsertNewUser.TabIndex = 6;
+            this.btnInsertNewUser.Text = "Insert new user";
+            this.btnInsertNewUser.UseVisualStyleBackColor = true;
+            this.btnInsertNewUser.Click += new System.EventHandler(this.btnInsertNewUser_Click);
+            // 
+            // lblOutputTest
+            // 
+            this.lblOutputTest.AutoSize = true;
+            this.lblOutputTest.Location = new System.Drawing.Point(231, 31);
+            this.lblOutputTest.Name = "lblOutputTest";
+            this.lblOutputTest.Size = new System.Drawing.Size(57, 13);
+            this.lblOutputTest.TabIndex = 7;
+            this.lblOutputTest.Text = "output test";
+            // 
+            // btnSelectCustomers
+            // 
+            this.btnSelectCustomers.Location = new System.Drawing.Point(218, 157);
+            this.btnSelectCustomers.Name = "btnSelectCustomers";
+            this.btnSelectCustomers.Size = new System.Drawing.Size(112, 37);
+            this.btnSelectCustomers.TabIndex = 8;
+            this.btnSelectCustomers.Text = "Select customers";
+            this.btnSelectCustomers.UseVisualStyleBackColor = true;
+            this.btnSelectCustomers.Click += new System.EventHandler(this.btnSelectCustomers_Click);
+            // 
+            // gridOutput
+            // 
+            this.gridOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridOutput.Location = new System.Drawing.Point(34, 220);
+            this.gridOutput.Name = "gridOutput";
+            this.gridOutput.Size = new System.Drawing.Size(476, 178);
+            this.gridOutput.TabIndex = 9;
+            this.gridOutput.SelectionChanged += new System.EventHandler(this.gridOutput_SelectionChanged);
+            // 
+            // gridEventOutput
+            // 
+            this.gridEventOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridEventOutput.Location = new System.Drawing.Point(532, 220);
+            this.gridEventOutput.Name = "gridEventOutput";
+            this.gridEventOutput.Size = new System.Drawing.Size(778, 152);
+            this.gridEventOutput.TabIndex = 10;
+            // 
+            // btnPerformerWindow
+            // 
+            this.btnPerformerWindow.Location = new System.Drawing.Point(22, 537);
+            this.btnPerformerWindow.Name = "btnPerformerWindow";
+            this.btnPerformerWindow.Size = new System.Drawing.Size(128, 34);
+            this.btnPerformerWindow.TabIndex = 11;
+            this.btnPerformerWindow.Text = "PerformerWindow";
+            this.btnPerformerWindow.UseVisualStyleBackColor = true;
+            this.btnPerformerWindow.Click += new System.EventHandler(this.btnPerformerWindow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1416, 600);
+            this.Controls.Add(this.btnPerformerWindow);
+            this.Controls.Add(this.gridEventOutput);
+            this.Controls.Add(this.gridOutput);
+            this.Controls.Add(this.btnSelectCustomers);
+            this.Controls.Add(this.lblOutputTest);
+            this.Controls.Add(this.btnInsertNewUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxAge);
+            this.Controls.Add(this.tbxEmail);
+            this.Controls.Add(this.tbxName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridOutput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEventOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.TextBox tbxAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsertNewUser;
+        private System.Windows.Forms.Label lblOutputTest;
+        private System.Windows.Forms.Button btnSelectCustomers;
+        private System.Windows.Forms.DataGridView gridOutput;
+        private System.Windows.Forms.DataGridView gridEventOutput;
+        private System.Windows.Forms.Button btnPerformerWindow;
     }
 }
 
--- EventBokning/Form1.cs
+++ EventBokning/Form1.cs
@@ -7,14 +7,141 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace EventBokning
 {
     public partial class Form1 : Form
     {
+
+        MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+
+            string server = "localhost";
+            string db = "mydb";
+            string user = "root";
+            string pw = "beepboop";
+
+            string connString = $"SERVER={server};DATABASE={db};UID={user};PASSWORD={pw};";
+
+            conn = new MySqlConnection(connString);
+        }
+
+        private void btnInsertNewUser_Click(object sender, EventArgs e)
+        {
+            InsertNewCustomerToDb();
+        }
+
+        private void InsertNewCustomerToDb()
+        {
+            string name = tbxName.Text;
+            string email = tbxEmail.Text;
+            int age = Convert.ToInt32(tbxAge.Text);
+
+            string query = $"CALL addCustomer('{name}', '{email}', {age});";
+
+            MySqlCommand sqlCmd = new MySqlCommand(query, conn);
+
+            try
+            {
+                conn.Open();
+                sqlCmd.ExecuteReader();
+                conn.Close();
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            MessageBox.Show("Insert successful");
+        }
+        private void selectCustomersFromDb()
+        {
+            string querry = "SELECT * FROM view_customers";
+            MySqlCommand sqlCmd = new MySqlCommand(querry, conn);
+            lblOutputTest.Text = "";
+            try
+            {
+                conn.Open();
+                // Skapa en datareader objekt som läsar våran datan. När connection stängs så försvinner datan.
+                MySqlDataReader reader = sqlCmd.ExecuteReader();
+
+                // datagrid
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                gridOutput.DataSource = dataTable;
+
+                // Förnya datan/kopplingen av readern.
+                reader = sqlCmd.ExecuteReader();
+
+                //While loop för att skriva ut hämtad data
+                while (reader.Read())
+                {
+                    string name = reader["name"].ToString();
+                    string email = reader["email"].ToString();
+                    int age = Convert.ToInt32(reader["age"]);
+
+                    lblOutputTest.Text += $"{name} har emailen {email} och är {age} år gammal.\n";
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void GetEventData(int id)
+        {
+            // Todo - If reader contains multiple rows of data, it searches for a primary key value(?) and if it can't find it, it overwrites previous data as it can't separare without a unique value.
+            // At the moment this prints out events.event_id(PK) in order to display multiple results.
+            // A solution would be to manually create the columns of the datatable instead of automatic loading, but it's a low priority fix.
+            string SQLquery = $"CALL getEventsOfCustomer({id});"; 
+            MySqlCommand sqlCmd = new MySqlCommand(SQLquery, conn);
+            try
+            {
+                conn.Open();
+                MySqlDataReader reader2 = sqlCmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader2);
+                gridEventOutput.DataSource = dt;
+
+                conn.Close();
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void getSelectedRow()
+        {
+            // Validering för att kontrollera att en rad har blivit hämtad
+            if (gridOutput.SelectedRows.Count != 1) return;
+
+            // Populate second datagrid of booked events of currently selected customer via id
+            int id = Convert.ToInt32(gridOutput.SelectedRows[0].Cells[0].Value);
+            GetEventData(id);
+        }
+
+        private void btnSelectCustomers_Click(object sender, EventArgs e)
+        {
+            selectCustomersFromDb();
+        }
+
+        private void gridOutput_SelectionChanged(object sender, EventArgs e)
+        {
+            getSelectedRow();
+        }
+
+        private void btnPerformerWindow_Click(object sender, EventArgs e)
+        {
+            Form2 PerformerWindow = new Form2();
+            this.Hide();
+            PerformerWindow.Show();
         }
     }
 }
